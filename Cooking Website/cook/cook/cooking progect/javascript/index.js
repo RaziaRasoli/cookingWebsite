468d7fbfa888c3e963329d49ce7d261c50ea0c62
RaziaRasoli <RaziRezawan@gmail.com>
2018-10-03 10:41:54 +0430

    First commit

---
+++ Cooking Website/cook/cook/cooking progect/javascript/index.js
@@ -0,0 +1,55 @@
+ // <script type="text/javascript">
+    function change_bread() {
+       window.location.assign("bread.html");
+       
+
+       
+    }
+    
+
+    
+    function change_ilo() {
+       window.location.assign("ilo.html");
+       
+    }
+   
+    
+    
+    function change_zarilo() {
+       window.location.assign("zarilo.html");
+       
+    }
+   
+    function change_omelat() {
+       window.location.assign("omelat.html");
+      
+    }
+   
+    function change_golabi() {
+       window.location.assign("golabi.html");
+      
+    }
+  
+
+    
+    function change_morabi_gerdo() {
+       window.location.assign("morabi-gerdo.html");
+       
+    }
+   
+
+    function change_toot() {
+       window.location.assign("toot.html");
+       
+    }
+    
+    function change_khroma() {
+       window.location.assign("pankik-khroma.html");
+      
+    }
+    
+     function change_omelat() {
+       window.location.assign("omelat.html");
+       
+    }
+    
